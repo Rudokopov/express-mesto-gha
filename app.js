71b7dc48c67dc4d83f64fb35acfd9c2fe73a88a7
Pavel Uzuniants <holabeda@gmail.com>
2023-05-08 17:31:19 +0300

    fix: cors disabled

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 import { router } from "./routes/index.js"
 import celebrate from "celebrate"
 import { handleErrors } from "./middlewares/handleErrors.js"
-import checkCors from "./middlewares/checkCORS.js"
+// import checkCors from "./middlewares/checkCORS.js"
 import { PORT, DATA_BASE } from "./config.js"
 
 import { requestLogger, errorLogger } from "./middlewares/logger.js"
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 mongoose.connect(DATA_BASE)
 
 app.use(requestLogger)
-app.use(checkCors)
+// app.use(checkCors)
 app.use(router)
 
 app.use(errorLogger)
